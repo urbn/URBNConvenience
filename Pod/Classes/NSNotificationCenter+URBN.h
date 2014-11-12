9eef44576ec997aff2cf33fa80018a9cdbdcbc7b
jgrandelli <jasongrandelli@gmail.com>
2014-11-12 14:51:57 -0500

    a little cleanup

--- Pod/Classes/NSNotificationCenter+URBN.h
+++ Pod/Classes/NSNotificationCenter+URBN.h
@@ -1,10 +1,3 @@
-//
-//  NSNotificationCenter+URBN.h
-//  URBNKit
-//
-//  Created by Brian Michel on 1/28/14.
-//  Copyright (c) 2014 URBN. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 
--- Pod/Classes/URBNConvenience.m
+++ Pod/Classes/URBNConvenience.m
@@ -1,10 +1,3 @@
-//
-//  URBNConvenience.m
-//  URBNConvenience
-//
-//  Created by Brian Michel on 1/28/14.
-//  Copyright (c) 2014 URBN. All rights reserved.
-//
 
 #import "URBNConvenience.h"
 
