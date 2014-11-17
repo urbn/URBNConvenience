d56732db0d72c3f8030bc791e82a5562eddca61e
jgrandelli <jasongrandelli@gmail.com>
2014-11-17 13:31:34 -0500

    getting rid of the silly brackets for frame access

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,4 +6,3 @@
 //  Copyright (c) 2014 jgrandelli. All rights reserved.
 //
 
-${TEST_EXAMPLE}
--- Pod/Classes/UIView+URBNLayout.m
+++ Pod/Classes/UIView+URBNLayout.m
@@ -8,61 +8,61 @@
 @implementation UIView (URBNLayout)
 
 - (CGPoint)position {
-	return [self frame].origin;
+	return self.frame.origin;
 }
 
 - (void)setPosition:(CGPoint)position {
-	CGRect rect = [self frame];
+	CGRect rect = self.frame;
 	rect.origin = position;
     [self setFrame:rect];
 }
 
 - (CGFloat)x {
-	return [self frame].origin.x;
+	return self.frame.origin.x;
 }
 
 - (void)setX:(CGFloat)x {
-	CGRect rect = [self frame];
+	CGRect rect = self.frame;
 	rect.origin.x = x;
     [self setFrame:rect];
 }
 
 - (CGFloat)y {
-	return [self frame].origin.y;
+	return self.frame.origin.y;
 }
 
 - (void)setY:(CGFloat)y {
-	CGRect rect = [self frame];
+	CGRect rect = self.frame;
 	rect.origin.y = y;
     [self setFrame:rect];
 }
 
 - (CGSize)size {
-	return [self frame].size;
+	return self.frame.size;
 }
 
 - (void)setSize:(CGSize)size {
-	CGRect rect = [self frame];
+	CGRect rect = self.frame;
 	rect.size = size;
     [self setFrame:rect];
 }
 
 - (CGFloat)width {
-	return [self frame].size.width;
+	return self.frame.size.width;
 }
 
 - (void)setWidth:(CGFloat)width {
-	CGRect rect = [self frame];
+	CGRect rect = self.frame;
 	rect.size.width = width;
     [self setFrame:rect];
 }
 
 - (CGFloat)height {
-	return [self frame].size.height;
+	return self.frame.size.height;
 }
 
 - (void)setHeight:(CGFloat)height {
-	CGRect rect = [self frame];
+	CGRect rect = self.frame;
 	rect.size.height = height;
     [self setFrame:rect];
 }
