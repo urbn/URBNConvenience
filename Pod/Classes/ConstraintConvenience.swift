6b1f98f64525f0eb1fbf2f9f0d73e1f13d42dfda
Nick DiStefano <NDistefano@urbn.com>
2015-12-11 08:46:31 -0500

    built with cocoapods master for swift compat bug

---
+++ Pod/Classes/ConstraintConvenience.swift
@@ -0,0 +1,20 @@
+//
+//  ConstraintConvenience.swift
+//  Pods
+//
+//  Created by Nick DiStefano on 12/11/15.
+//
+//
+
+import Foundation
+
+public func activateVFL(format: String, options: NSLayoutFormatOptions = [], metrics: [String : AnyObject]? = nil, views: [String : AnyObject]) {
+    NSLayoutConstraint.activateConstraints(
+        NSLayoutConstraint.constraintsWithVisualFormat(
+            format,
+            options: options,
+            metrics: metrics,
+            views: views
+        )
+    )
+}
