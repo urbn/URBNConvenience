794879d5e68a867060026e195dbf87e562dbe6ad
Nick DiStefano <NDistefano@urbn.com>
2015-12-12 07:19:44 -0500

    update searchable protocol ot use optionset

--- Pod/Classes/SearchableProtocol.swift
+++ Pod/Classes/SearchableProtocol.swift
@@ -1,31 +1,18 @@
 //
 //  SearchableProtocol.swift
-//  Pods
+//  ANT
 //
-//  Created by Nick DiStefano on 12/11/15.
-//
+//  Created by Nick DiStefano on 12/10/15.
+//  Copyright © 2015 Urban Outfitters. All rights reserved.
 //
 
 import Foundation
 
-enum SearchableElegibility {
-    case SearchHandoff
-    case SearchHandoffPublicIndexing
-    
-    func eligibleForSearch() -> Bool {
-        return true
-    }
-    
-    func elegibleForHandoff() -> Bool {
-        return true
-    }
-    
-    func elegibleForPublicIndexing() -> Bool {
-        switch self {
-        case .SearchHandoff: return false
-        case .SearchHandoffPublicIndexing: return true
-        }
-    }
+struct UserActivityEligibility : OptionSetType {
+    let rawValue : Int
+    static let Search = UserActivityEligibility(rawValue: 1 << 1)
+    static let Handoff = UserActivityEligibility(rawValue: 1 << 2)
+    static let PublicIndexing = UserActivityEligibility(rawValue: 1 << 3)
 }
 
 @available(iOS 9.0, *)
@@ -34,20 +21,20 @@ protocol Searchable {
     func searchTitle() -> String
     func keywords() -> Set<String>
     func contentDescription() -> String
-    func configActivity(searchActivity: NSUserActivity, elegibility: SearchableElegibility) -> NSUserActivity
+    func configActivity(searchActivity: NSUserActivity, eligibilityOptions: UserActivityEligibility) -> NSUserActivity
 }
 
 @available(iOS 9.0, *)
 extension Searchable {
-    func configActivity(searchActivity: NSUserActivity, elegibility: SearchableElegibility) -> NSUserActivity {
+    func configActivity(searchActivity: NSUserActivity, eligibilityOptions: UserActivityEligibility) -> NSUserActivity {
         searchActivity.title = searchTitle()
         searchActivity.webpageURL = NSURL(string: searchURL())
         searchActivity.keywords = keywords()
         searchActivity.contentAttributeSet = searchableAttributeSet()
         
-        searchActivity.eligibleForSearch = elegibility.eligibleForSearch()
-        searchActivity.eligibleForHandoff = elegibility.elegibleForHandoff()
-        searchActivity.eligibleForPublicIndexing = elegibility.elegibleForPublicIndexing()
+        searchActivity.eligibleForSearch = eligibilityOptions.contains(.Search)
+        searchActivity.eligibleForHandoff = eligibilityOptions.contains(.Handoff)
+        searchActivity.eligibleForPublicIndexing = eligibilityOptions.contains(.PublicIndexing)
         
         searchActivity.becomeCurrent()
         
