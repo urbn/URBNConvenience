df8dfc6f90d906ffb3e2a2727ed66b712cba53f3
Nick DiStefano <NDistefano@urbn.com>
2015-12-13 09:56:48 -0500

    separate info providing into seperate protocol

--- Pod/Classes/SearchableProtocol.swift
+++ Pod/Classes/SearchableProtocol.swift
@@ -17,42 +17,46 @@ struct UserActivityEligibility : OptionSetType {
     static let PublicIndexing = UserActivityEligibility(rawValue: 1 << 3)
 }
 
-@available(iOS 9.0, *)
-protocol Searchable {
+protocol SearchInfoProvider {
+    var userActivityEligibility: UserActivityEligibility { get }
     func searchURL() -> String
     func searchTitle() -> String
     func keywords() -> Set<String>
     func contentDescription() -> String
-    func configActivity(searchActivity: NSUserActivity, eligibilityOptions: UserActivityEligibility) -> NSUserActivity
+}
+
+@available(iOS 9.0, *)
+protocol Searchable {
+    func configActivity(searchActivity: NSUserActivity, infoProvider: SearchInfoProvider) -> NSUserActivity
 }
 
 @available(iOS 9.0, *)
 extension Searchable {
-    func configActivity(searchActivity: NSUserActivity, eligibilityOptions: UserActivityEligibility) -> NSUserActivity {
-        searchActivity.title = searchTitle()
-        searchActivity.webpageURL = NSURL(string: searchURL())
-        searchActivity.keywords = keywords()
-        searchActivity.contentAttributeSet = searchableAttributeSet()
+    func configActivity(searchActivity: NSUserActivity, infoProvider: SearchInfoProvider) -> NSUserActivity {
+        searchActivity.title = infoProvider.searchTitle()
+        searchActivity.webpageURL = NSURL(string: infoProvider.searchURL())
+        searchActivity.keywords = infoProvider.keywords()
+        searchActivity.contentAttributeSet = searchableAttributeSet(infoProvider)
         
-        searchActivity.eligibleForSearch = eligibilityOptions.contains(.Search)
-        searchActivity.eligibleForHandoff = eligibilityOptions.contains(.Handoff)
-        searchActivity.eligibleForPublicIndexing = eligibilityOptions.contains(.PublicIndexing)
+        searchActivity.eligibleForSearch = infoProvider.userActivityEligibility.contains(.Search)
+        searchActivity.eligibleForHandoff = infoProvider.userActivityEligibility.contains(.Handoff)
+        searchActivity.eligibleForPublicIndexing = infoProvider.userActivityEligibility.contains(.PublicIndexing)
         
         searchActivity.becomeCurrent()
         
         return searchActivity
     }
     
-    private func searchableAttributeSet() -> CSSearchableItemAttributeSet {
+    private func searchableAttributeSet(infoProvider: SearchInfoProvider) -> CSSearchableItemAttributeSet {
         let attributes = CSSearchableItemAttributeSet(itemContentType: kUTTypeItem as String)
         
-        attributes.title = searchTitle()
-        attributes.displayName = searchTitle()
+        attributes.title = infoProvider.searchTitle()
+        attributes.displayName = infoProvider.searchTitle()
         
-        attributes.relatedUniqueIdentifier = searchURL()
-        attributes.contentDescription = contentDescription()
+        attributes.relatedUniqueIdentifier = infoProvider.searchURL()
+        attributes.contentDescription = infoProvider.contentDescription()
         
-        attributes.keywords = Array(keywords())
+        attributes.keywords = Array(infoProvider.keywords())
         
         return attributes
     }
