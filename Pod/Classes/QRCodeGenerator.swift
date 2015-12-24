3da3058f6bf631e5106dd416f0a9a4baa8d47f8a
Nick DiStefano <NDistefano@urbn.com>
2015-12-24 09:01:02 -0500

    try it with a different spin from the func swift book

--- Pod/Classes/QRCodeGenerator.swift
+++ Pod/Classes/QRCodeGenerator.swift
@@ -9,8 +9,18 @@
 import Foundation
 
 
+public typealias Filter = CIImage? -> CIImage?
+
+infix operator >>> { associativity left }
+
+func >>> ( Filter1 : Filter , Filter2 : Filter ) -> Filter {
+    return { image in Filter2 ( Filter1 (image)) }
+}
+
 public func qrImage(text: String, foregroundColor: UIColor, backgroundColor: UIColor, size: CGSize) -> UIImage? {
-    return qrImage(text)?.scale(size)?.color(foregroundColor: foregroundColor, backgroundColor: backgroundColor)?.mapToUIImage()
+    let qrFilter = scale(size) >>> falseColor(foregroundColor: foregroundColor, backgroundColor: backgroundColor)
+
+    return qrFilter(qrImage(text))?.mapToUIImage()
 }
 
 public func qrImage(text: String) -> CIImage? {
@@ -23,25 +33,33 @@ public func qrImage(text: String) -> CIImage? {
     return filter?.outputImage
 }
 
-public extension CIImage {
-    public func mapToUIImage() -> UIImage? {
-        return UIImage(CIImage: self)
-    }
-    
-    public func scale(size: CGSize) -> CIImage? {
-        let scaleX = size.width / extent.size.width
-        let scaleY = size.height / extent.size.height
+public func scale(size: CGSize) -> Filter {
+    return { image in
+        guard let imageToScale = image else { return nil }
+
+        let scaleX = size.width / imageToScale.extent.size.width
+        let scaleY = size.height / imageToScale.extent.size.height
         
-        return imageByApplyingTransform(CGAffineTransformMakeScale(scaleX, scaleY))
+        return imageToScale.imageByApplyingTransform(CGAffineTransformMakeScale(scaleX, scaleY))
     }
-    
-    public func color(foregroundColor foregroundColor: UIColor, backgroundColor: UIColor) -> CIImage? {
+}
+
+public func falseColor(foregroundColor foregroundColor: UIColor, backgroundColor: UIColor) -> Filter {
+    return { image in
+        guard let imageToColor = image else { return nil }
+        
         let foregroundCoreColor = convertToCIColor(foregroundColor)
         let backgroundCoreColor = convertToCIColor(backgroundColor)
         
-        let colorFilter = CIFilter(name: "CIFalseColor", withInputParameters: ["inputImage": self, "inputColor0":foregroundCoreColor, "inputColor1":backgroundCoreColor])
+        let colorFilter = CIFilter(name: "CIFalseColor", withInputParameters: ["inputImage": imageToColor, "inputColor0":foregroundCoreColor, "inputColor1":backgroundCoreColor])
         
         return colorFilter?.outputImage
+    }
+}
+
+public extension CIImage {
+    public func mapToUIImage() -> UIImage? {
+        return UIImage(CIImage: self)
     }
 }
 
