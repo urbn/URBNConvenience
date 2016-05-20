1f0bf3e31055931d1b609b8994182e15e022d9ee
Matt Thomas <matt@codecaffeine.com>
2016-05-20 15:11:08 -0400

    cleanup

--- Pod/Classes/DictionaryConvenience.swift
+++ Pod/Classes/DictionaryConvenience.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public func += <Key, Value> (inout left: Dictionary<Key, Value>, right: Dictionary<Key, Value>) {
+public func += <Key, Value> (inout left: [Key: Value], right: [Key: Value]) {
     for (key, value) in right {
         left[key] = value
     }
