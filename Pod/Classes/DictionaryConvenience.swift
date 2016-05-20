f79f70ae270d9b04552675a209cdf4acea55a1c7
Matt Thomas <matt@codecaffeine.com>
2016-05-20 14:45:40 -0400

    😁

--- Pod/Classes/DictionaryConvenience.swift
+++ Pod/Classes/DictionaryConvenience.swift
@@ -10,6 +10,6 @@ import Foundation
 
 public func += <Key, Value> (inout left: Dictionary<Key, Value>, right: Dictionary<Key, Value>) {
     for (key, value) in right {
-        left.updateValue(value, forKey: key)
+        left[key] = value
     }
 }
