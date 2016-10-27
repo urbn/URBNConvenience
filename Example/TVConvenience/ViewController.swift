df4a4d0c1bb9ce5359c48d5ad83cd41646f1bfda
dutcher <Dutcher@users.noreply.github.com>
2016-10-27 09:58:36 -0400

    Added tvOS target

---
+++ Example/TVConvenience/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVConvenience
+//
+//  Created by Evan Dutcher on 10/27/16.
+//  Copyright © 2016 jgrandelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
