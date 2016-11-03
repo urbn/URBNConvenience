23c7c57962d6f726507eb3d07856db81ca98921c
Ray Migneco <ray.migneco@gmail.com>
2016-11-03 12:25:22 -0400

    Added deprecation syntax

--- Pod/Classes/NSNotificationCenter+URBN.h
+++ Pod/Classes/NSNotificationCenter+URBN.h
@@ -3,8 +3,8 @@
 
 @interface NSNotificationCenter (MainQueue)
 
-- (void)urbn_postNotification:(NSNotification *)notification onMainQueue:(BOOL)onMainQueue;
-- (void)urbn_postNotificationName:(NSString *)aName object:(id)anObject onMainQueue:(BOOL)onMainQueue;
-- (void)urbn_postNotificationName:(NSString *)aName object:(id)anObject userInfo:(NSDictionary *)aUserInfo onMainQueue:(BOOL)onMainQueue;
+- (void)urbn_postNotification:(NSNotification *)notification onMainQueue:(BOOL)onMainQueue __attribute((deprecated("Use methods in NotificationCenter.swift")));
+- (void)urbn_postNotificationName:(NSString *)aName object:(id)anObject onMainQueue:(BOOL)onMainQueue __attribute((deprecated("Use methods in NotificationCenter.swift")));
+- (void)urbn_postNotificationName:(NSString *)aName object:(id)anObject userInfo:(NSDictionary *)aUserInfo onMainQueue:(BOOL)onMainQueue __attribute((deprecated("Use methods in NotificationCenter.swift")));
 
 @end
