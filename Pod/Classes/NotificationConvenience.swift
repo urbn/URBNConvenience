5a689549dcd6a3d1b236c6ee8af61378e7f2cc99
Ray Migneco <ray.migneco@gmail.com>
2016-11-03 10:41:54 -0400

    Fix typos
    - function  renamed

--- Pod/Classes/NotificationConvenience.swift
+++ Pod/Classes/NotificationConvenience.swift
@@ -12,15 +12,15 @@ import Foundation
 public extension NotificationCenter {
     
     public func post(notification: Notification, onMainQueue mainQueue: Bool) -> Void {
-        func post() {
+        func postNote() {
             post(notification)
         }
         
-        mainQueue ? DispatchQueue.main.async { post() } : post()
+        mainQueue ? DispatchQueue.main.async { postNote() } : postNote()
     }
     
     public func post(notificationName: String, onMainQueue mainQueue: Bool, object: Any? = nil, userInfo: [AnyHashable: Any]? = nil) -> Void {
-        let note = Notification(name: Notification.Name(rawValue: notificaitonName), object: object, userInfo: info)
+        let note = Notification(name: Notification.Name(rawValue: notificationName), object: object, userInfo: userInfo)
         post(notification: note, onMainQueue: mainQueue)
     }
 }
